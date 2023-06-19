f62456d90aa63dc33a375185529e316f0161c104
wineshuga <uzochukwuwinnie@gmail.com>
2023-06-19 11:21:32 +0100

    Revert "Add querySelectors to index.js file"
    This reverts commit 780094f0f63f9c0a6b6977bec6b788af1e8e903f.

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 const fetch = require('node-fetch/src/index.js');
-
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-const closeBtn = document.querySelector('.close-button');
-const links = document.querySelectorAll('.links');
