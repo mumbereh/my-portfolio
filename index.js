2f16b9297842d03ac94cbc1d865db12a93decd8c
wineshuga <uzochukwuwinnie@gmail.com>
2023-06-19 11:50:42 +0100

    Add querySelectors to index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+const closeBtn = document.querySelector('.close-button');
+const links = document.querySelectorAll('.links');
