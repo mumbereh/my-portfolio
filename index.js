780094f0f63f9c0a6b6977bec6b788af1e8e903f
wineshuga <uzochukwuwinnie@gmail.com>
2023-06-19 11:13:38 +0100

    Add querySelectors to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const fetch = require('node-fetch/src/index.js');
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+const closeBtn = document.querySelector('.close-button');
+const links = document.querySelectorAll('.links');
