8bdc3b853935116d369d28d5a6fa4dcac6275555
mumbereh <siirasonh@gmail.com>
2023-06-20 00:10:34 +0300

    Modified index.html, index.js & Popup.css

--- index.js
+++ index.js
@@ -24,3 +24,140 @@ links.forEach((n) => n.addEventListener('click', () => {
   nav.classList.remove('show');
   menuToggle.style.display = 'block';
 }));
+
+const project = [
+  {
+    src: 'images/61.png',
+    title: 'Tonic',
+    description: [
+      'A daily selection of privately personalized reads no accounts or sign-ups required',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.',
+    ],
+    details: 'See project',
+    live: 'https://mumbereh.github.io/First-portfolio',
+    source: 'https://github.com/mumbereh/First-portfolio',
+  },
+  {
+    src: 'images/4.png',
+    title: 'Multi-Post Stories',
+    description: [
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    ],
+    details: 'See project',
+    live: 'https://mumbereh.github.io/canopy-website',
+    source: 'https://github.com/mumbereh/canopy-website',
+  },
+  {
+    src: 'images/1.png',
+    title: 'Facebook 360',
+    description: [
+      'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    ],
+    details: 'See project',
+    live: 'https://mumbereh.github.io/Swampflix',
+    source: 'https://github.com/mumbereh/Swampflix',
+  },
+  {
+    src: 'images/2.png',
+    title: 'Uber Navigation',
+    description: [
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    ],
+    details: 'See project',
+    live: 'https://mumbereh.github.io/Lambada',
+    source: 'https://github.com/mumbereh/Lambada',
+  },
+];
+
+const proContainer = document.querySelector('.pro-container');
+
+for (let i = 0; i < project.length; i += 1) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+    <div class="card">
+      <div class="pic">
+        <img src="${project[i].src}" alt="">
+      </div>
+      <div>
+        <h2 class="title">${project[i].title}</h2>
+        <ul class="field">
+          <li class="cano">FACEBOOK</li>
+          <li class="tag">&#8226; Full Stack Dev </li>
+          <li class="tag">&#8226; 2015</li>
+        </ul>
+        <p class="desk-praa">${project[i].description[0]}</p>
+        <ul class="trainning">
+          <li class="other-skills">html</li>
+          <li class="ruby">Ruby on Rails</li>
+          <li class="other-skills">css</li>
+          <li class="other-skills">javascript</li>
+        </ul>
+        <button class="card-btn">${project[i].details}</button>
+      </div>
+    </div>
+  `;
+
+  proContainer.appendChild(card);
+}
+
+const popupModal = document.querySelector('#popup-modal');
+const cardBtn = document.querySelectorAll('.card-btn');
+const popupContainer = document.querySelector('.popup');
+
+project.forEach((projectItem, cardIndex) => {
+  cardBtn.forEach((btn, btnIndex) => {
+    btn.addEventListener('click', () => {
+      if (btnIndex === cardIndex) {
+        popupModal.style.display = 'block';
+        popupModal.innerHTML = `
+          <div class="popup">
+            <h2 class="pop-title">${project[cardIndex].title}</h2>
+            <ul class="pop-field">
+              <li class="cano">CANOPY</li>
+              <li class="tag">&#8226; Back End Dev</li>
+              <li class="tag">&#8226; 2015</li>
+            </ul>
+            <div class="pop-img-container">
+              <img class="pop-img" src="${project[cardIndex].src}" alt="">
+            </div>
+            <div class="pop-para">
+              <p>${project[cardIndex].description[0]}</p>
+            </div>
+            <div class="pop-skills">
+              <ul class="pop-training">
+                <li class="other-skills">HTML</li>
+                <li class="other-skills">CSS</li>
+                <li class="other-skills">JavaScript</li>
+                <li class="other-skills">Ruby</li>
+                <li class="other-skills">Bootstrap</li>
+              </ul>
+            </div>
+            <div class="pop-links">
+              <a href="${project[cardIndex].live}">
+                <button class="pop-link1 pop-card-btn" type="submit">
+                  See live
+                  <img class="search" src="pop-images/live.png" alt="">
+                </button>
+              </a>
+              <a href="${project[cardIndex].source}">
+                <button class="pop-link2 pop-card-btn" type="submit">
+                  See source
+                  <img class="search" src="pop-images/popup.png" alt="">
+                </button>
+              </a>
+            </div>
+            <p id="close-btn">&times;</p>
+          </div>
+        `;
+      }
+
+      const closebtn = document.querySelector('#close-btn');
+      closebtn.addEventListener('click', () => {
+        popupModal.style.display = 'none';
+        popupContainer.style.display = 'none';
+      });
+    });
+  });
+});
